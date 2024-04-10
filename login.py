4a52e7184d7cc759acb743044939ec813d6dcfe5
BATOER-0605 <nem0605@outlook.com>
2024-04-10 23:51:09 +0900

    first

--- login.py
+++
@@ -1,7 +0,0 @@
-import userLoginLib
-
-uname = input("ユーザー名:")
-passwd = input("パスワード")
-
-userLoginLib.user_login_console(uname=uname,passwd=passwd)
-print("実行完了")
--- test.py
+++
@@ -1,7 +0,0 @@
-import dbc,userLoginLib
-
-
-res = dbc.search_userinfo_from_name("齋藤直人")
-
-print(res[2])
-print(type(res[0]))
