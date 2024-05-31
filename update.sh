e130c328a83b0d18e71e2581ad8dbb5f9c07b749
BATOER-0605 <nem0605@outlook.com>
2024-06-01 05:46:26 +0900

    データ復元の手順を追加

--- update.sh
+++ update.sh
@@ -5,4 +5,6 @@ docker cp pcc-rent:/PCC-RENT/pcc-rent.db ../pcc-rent-Backup
 docker cp pcc-rent:/PCC-RENT/setting_files ../pcc-rent-Backup
 echo "****Use GitHub Account which joined ANCT-PCC Organization !****"
 git pull
+docker cp ../pcc-rent-Backup/pcc-rent.db pcc-rent:/PCC-RENT
+docker cp ../pcc-rent-Backup/setting_files pcc-rent:/PCC-RENT
 ./reinstall.sh
