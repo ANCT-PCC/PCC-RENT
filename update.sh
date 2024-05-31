630ee4da2dde62949c6af71aa54be1361c86956f
BATOER-0605 <nem0605@outlook.com>
2024-06-01 05:08:13 +0900

    up

--- update.sh
+++ update.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+mkdir ../pcc-rent-Backup
 docker cp pcc-rent:/PCC-RENT/pcc-rent.db ../pcc-rent-Backup
 docker cp pcc-rent:/PCC-RENT/setting_files ../pcc-rent-Backup
 echo "****Use GitHub Account which joined ANCT-PCC Organization !****"
