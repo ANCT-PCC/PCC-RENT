220bbfebffe3c335ca5749d78e1c4592ee65057f
BATOER-0605 <nem0605@outlook.com>
2024-06-01 05:53:37 +0900

    up

--- update.sh
+++ update.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+rm -r ../pcc-rent-Backup
 mkdir ../pcc-rent-Backup
 docker cp pcc-rent:/PCC-RENT/pcc-rent.db ../pcc-rent-Backup
 docker cp pcc-rent:/PCC-RENT/setting_files ../pcc-rent-Backup
