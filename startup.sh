b1c40761fc062c20264a58522ad3ce0d9d89b86b
BATOER-0605 <nem0605@outlook.com>
2024-06-01 04:48:20 +0900

    アップデートシステムの更新

--- startup.sh
+++ startup.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
+PREV_ADDR = 'http://localhost:8080/'
+SERVER_ADDR = 'http://192.168.200.100:8080/'
 
-sed -i -e s#http://localhost:8080/#http://192.168.200.100:8080/#g static/login.js
-sed -i -e s#http://localhost:8080/#http://192.168.200.100:8080/#g static/admin-db.js
-sed -i -e s#http://localhost:8080/#http://192.168.200.100:8080/#g static/admin-user.js
-sed -i -e s#http://localhost:8080/#http://192.168.200.100:8080/#g static/admin-item.js
-sed -i -e s#http://localhost:8080/#http://192.168.200.100:8080/#g static/admin-top.js
-sed -i -e s#http://localhost:8080/#http://192.168.200.100:8080/#g static/admintools.js
-sed -i -e s#http://localhost:8080/#http://192.168.200.100:8080/#g static/dashboard.js
-sed -i -e s#http://localhost:8080/#http://192.168.200.100:8080/#g static/members.js
-sed -i -e s#http://localhost:8080/#http://192.168.200.100:8080/#g static/my_rental_list.js
-sed -i -e s#http://localhost:8080/#http://192.168.200.100:8080/#g static/passwd_change.js
-sed -i -e s#http://localhost:8080/#http://192.168.200.100:8080/#g static/pcc-items.js
-sed -i -e s#http://localhost:8080/#http://192.168.200.100:8080/#g static/user_settings.js
+sed -i -e s#$PREV_ADDR#$SERVER_ADDR#g static/login.js
+sed -i -e s#$PREV_ADDR#$SERVER_ADDR#g static/admin-db.js
+sed -i -e s#$PREV_ADDR#$SERVER_ADDR#g static/admin-user.js
+sed -i -e s#$PREV_ADDR#$SERVER_ADDR#g static/admin-item.js
+sed -i -e s#$PREV_ADDR#$SERVER_ADDR#g static/admin-top.js
+sed -i -e s#$PREV_ADDR#$SERVER_ADDR#g static/admintools.js
+sed -i -e s#$PREV_ADDR#$SERVER_ADDR#g static/dashboard.js
+sed -i -e s#$PREV_ADDR#$SERVER_ADDR#g static/members.js
+sed -i -e s#$PREV_ADDR#$SERVER_ADDR#g static/my_rental_list.js
+sed -i -e s#$PREV_ADDR#$SERVER_ADDR#g static/passwd_change.js
+sed -i -e s#$PREV_ADDR#$SERVER_ADDR#g static/pcc-items.js
+sed -i -e s#$PREV_ADDR#$SERVER_ADDR#g static/user_settings.js
 
 python run.py
--- update.sh
+++ update.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-cp pcc-rent.db ../pcc-rent-DB-backup/
+docker cp pcc-rent:/PCC-RENT/pcc-rent.db ../pcc-rent-Backup/pcc-rent.db
+docker cp pcc-rent:/PCC-RENT/setting_files ../pcc-rent-Backup/setting_files
 echo "****Use GitHub Account which joined ANCT-PCC Organization !****"
 git pull
 ./reinstall.sh
